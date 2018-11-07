fa68e6320117253f53e4ff0fa4781949d3ccd438
Andrew Tzeng <tzeng.a@gmail.com>
2018-11-07 15:22:13 -0500

    Naive Bayes work part 1

---
+++ naiveBayes/NB_terrain/ClassifyNB.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+def classify(features_train, labels_train):   
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    
+    clf = GaussianNB()
+    clf_fit = clf.fit(features_train, labels_train)
+    
+    return clf_fit
+    
+
+    
